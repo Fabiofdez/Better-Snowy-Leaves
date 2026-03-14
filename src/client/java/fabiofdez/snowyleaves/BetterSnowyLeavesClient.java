0d8f0a08cf17073d9d0576f15407507eeded7810
Fabio Fernandez <ffernan9@jhu.edu>
2026-03-13 21:20:17 -0500

    add new mod resources, ++version > new block models, textures > handle new blockstates > built-in resource packs > add "Stay True" compat > update mod icon

--- src/client/java/fabiofdez/snowyleaves/BetterSnowyLeavesClient.java
+++ src/client/java/fabiofdez/snowyleaves/BetterSnowyLeavesClient.java
@@ -1,10 +1,31 @@
 package fabiofdez.snowyleaves;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.network.chat.Component;
 
 public class BetterSnowyLeavesClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+  @Override
+  public void onInitializeClient() {
+    ModContainer container = FabricLoader
+        .getInstance()
+        .getModContainer(BetterSnowyLeaves.MOD_ID)
+        .orElseThrow();
+
+    ResourceManagerHelper.registerBuiltinResourcePack(
+        BetterSnowyLeaves.id("default"),
+        container,
+        Component.literal("Default Snowy leaves"),
+        ResourcePackActivationType.DEFAULT_ENABLED
+    );
+    ResourceManagerHelper.registerBuiltinResourcePack(
+        BetterSnowyLeaves.id("stay_true_compat"),
+        container,
+        Component.literal("Snowy leaves X Stay True"),
+        ResourcePackActivationType.NORMAL
+    );
+  }
 }
